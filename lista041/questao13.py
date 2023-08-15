df5428a5cdea0c7639d9c9f05ced5737355f4e3b
Camila Almeida <camilaalma2008@gmail.com>
2023-08-15 11:55:56 -0300

    ultimas alterações feitas

--- lista041/questao13.py
+++ lista041/questao13.py
@@ -7,6 +7,30 @@ a = int(input("Insira um número inteiro:"))
 b = int(input("Insira outro número inteiro:"))
 c = int(input("Insira mais um número inteiro:"))
 
+# 1- a tem que ser menor que b
+if ( a > b ):
+    aux = a
+    a = b
+    b = aux
+
+# 2- a tem que ser menor que c
+if (a > c):
+    aux = a
+    a = c
+    c = aux
+
+# garantido até aqui que a é o menor dos 3
+# agora é necessário garantir que b seja menor que c
+if (b > c):
+    aux = b
+    b = c
+    b = aux
+
+# garantido até aqui que entre b e c, o b é menor, ou seja, o c é o maior de todos
+
+print(f"Ordem crescente: {a}, {b} e {c}")
+
+'''
 if a <= b and a <= c and b <= c:
     print(f'A ordem crescente é {a}, {b} e {c}')
 elif a <= b and a <=c and c <= b:
@@ -19,3 +43,4 @@ elif c <= a and c <= b and a <=b:
     print(f'A ordem crescente é {c}, {a} e {b}')
 elif c <= a and c <= b and b <= a:
     print(f'A ordem crescente é {c}, {b} e {a}')
+'''
