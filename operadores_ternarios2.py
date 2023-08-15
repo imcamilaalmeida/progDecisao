9d07f970a9093cc486c13b42af92ad5727d12ca3
Camila Almeida <camilaalma2008@gmail.com>
2023-08-15 11:22:51 -0300

    Aprendendo sobre operadores ternarios

---
+++ operadores_ternarios2.py
@@ -0,0 +1,11 @@
+'''
+Crie um programa que pergunte a idade do usuário e em seguida informe se este usuário é menor de idade ou maior de idade.
+'''
+
+idade = int(input("Informe sua idade: "))
+
+#logica do op ternario2: var = resultado_verdadeiro if teste_condicional else resultado_falso
+
+resposta = "Você é maior de idade!" if idade>=18 else "Você é menor de idade!"
+
+print(resposta)
