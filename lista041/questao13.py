a81bd62c6bb527496fb64d1ffe369f8dd5620ec2
Camila Almeida <camilaalma2008@gmail.com>
2023-08-15 10:51:16 -0300

    Ultimas adaptações das atividades de decisão!

--- lista041/questao13.py
+++ lista041/questao13.py
@@ -7,26 +7,15 @@ a = int(input("Insira um número inteiro:"))
 b = int(input("Insira outro número inteiro:"))
 c = int(input("Insira mais um número inteiro:"))
 
-maior = a
-
-if (maior < b):
-    maior = b
-
-if (maior < c):
-    maior = c
-
-menor = a
-
-if (menor > b):
-    maior = b
-
-if (menor > c):
-    maior = c
-
-medio = a
-
-if (medio < menor and medio < maior):
-    medio = b
-
-if (maior < c):
-    maior = c
+if a <= b and a <= c and b <= c:
+    print(f'A ordem crescente é {a}, {b} e {c}')
+elif a <= b and a <=c and c <= b:
+    print(f'A ordem crescente é {a}, {c} e {b}')
+elif b <= a and b <= c and a <= c:
+    print(f'A ordem crescente é {b}, {a} e {c}')
+elif b <= a and b <= c and c <= a:
+    print(f'A ordem crescente é {b}, {c} e {a}')
+elif c <= a and c <= b and a <=b:
+    print(f'A ordem crescente é {c}, {a} e {b}')
+elif c <= a and c <= b and b <= a:
+    print(f'A ordem crescente é {c}, {b} e {a}')
